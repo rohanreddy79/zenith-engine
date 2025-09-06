68259bd3d34af75674082ad8990cc0c8b8099b94
Rohan Reddy Jakkam <86797493+rohanreddy79@users.noreply.github.com>
2025-09-06 09:30:51 -0400

    refactor(engine): extract worker supervisor into dedicated coordinator

--- src/zenith/core/engine.py
+++ src/zenith/core/engine.py
@@ -4241,3 +4241,6 @@ class AsyncEngine:
                 self._active_tasks[task_id].cancel()
                 return True
             return False
+    @property
+    def supervisor(self) -> 'WorkerSupervisor':
+        return self._supervisor
