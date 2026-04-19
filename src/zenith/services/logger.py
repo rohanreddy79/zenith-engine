29dc995a282fc99ec8b1e9810bf900d66b9b0ed4
Rohan Reddy Jakkam <86797493+rohanreddy79@users.noreply.github.com>
2026-04-19 09:10:10 -0400

    feat(logger): add structured JSON logging with dynamic correlation IDs

--- src/zenith/services/logger.py
+++ src/zenith/services/logger.py
@@ -1393,3 +1393,6 @@ class StructuredLogger:
     def info(self, msg: str, **kwargs) -> None:
         payload = {'msg': msg, 'correlation_id': self.correlation_id, **kwargs}
         self._sink.write(json.dumps(payload) + '\n')
+    def info(self, msg: str, **kwargs) -> None:
+        payload = {'msg': msg, 'correlation_id': self.correlation_id, **kwargs}
+        self._sink.write(json.dumps(payload) + '\n')
