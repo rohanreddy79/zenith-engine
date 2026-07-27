ac148c93eba17e8da5de7f91ee65589e217b64a7
Rohan Reddy Jakkam <86797493+rohanreddy79@users.noreply.github.com>
2026-07-27 13:52:40 -0400

    refactor(engine): extract worker supervisor into dedicated coordinator

--- src/zenith/core/engine.py
+++ src/zenith/core/engine.py
@@ -11275,3 +11275,6 @@ class AsyncEngine:
     @property
     def supervisor(self) -> 'WorkerSupervisor':
         return self._supervisor
+    @property
+    def supervisor(self) -> 'WorkerSupervisor':
+        return self._supervisor
